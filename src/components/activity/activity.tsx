13eecdd3c2918b571f467fd1ef2aa121a932cdf0
martínval11 <martinvaldez.dev@gmail.com>
2024-04-30 19:29:19 -0300

    delete and edit activity (no functionality nor components)

--- src/components/activity/activity.tsx
+++ src/components/activity/activity.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import clsx from 'clsx';
 
 import { useStore } from '@/lib/zStates';
@@ -6,21 +6,32 @@ import { useStore } from '@/lib/zStates';
 import {
   Card,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
 
 import type { Activity } from '@/types/types';
 
 export function Activity() {
-  const { activityArray, addActivity } = useStore();
+  const { activityArray } = useStore();
+  const [activityState, setActivityState] = useState(activityArray);
+
+  const removeActivityCard = (id: number) => {
+    setActivityState((prev) => prev.filter((item) => item.id !== id));
+    localStorage.setItem(
+      'data',
+      JSON.stringify(activityArray.filter((item) => item.id !== id))
+    );
+  };
 
   useEffect(() => {
     const localData = localStorage.getItem('data');
 
     if (localData) {
-      activityArray.push(...JSON.parse(localData));
-      addActivity();
+      const parsedData = JSON.parse(localData);
+      setActivityState([...parsedData]);
     }
   }, []);
 
@@ -29,7 +40,7 @@ export function Activity() {
       <h2 className="mb-3 text-lg font-bold">Activity</h2>
 
       <div className="flex flex-col gap-4">
-        {activityArray.map((item: Activity) => (
+        {activityState.map((item: Activity) => (
           <Card key={item.id}>
             <CardHeader
               className={clsx(
@@ -49,6 +60,19 @@ export function Activity() {
                 {item.type === 'income' ? '+' : '-'} ${item.quantity}
               </CardDescription>
             </CardHeader>
+            <CardFooter className="flex gap-2">
+              <Button variant="outline">Edit</Button>
+              <Button
+                id={item.id + ''}
+                onClick={(event) => {
+                  const target = event.target as HTMLButtonElement;
+                  removeActivityCard(Number(target.id));
+                }}
+                variant="secondary"
+              >
+                Remove
+              </Button>
+            </CardFooter>
           </Card>
         ))}
       </div>
